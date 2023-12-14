3b4f969478b83c49807fb65c2fcdc3f259e26230
katewee <seoyoungwee@gmail.com>
2023-12-14 18:43:21 +0900

    create project

---
+++ src/main/java/com/mwamwayababmeokuh/mwamwaspring/MwamwaSpringApplication.java
@@ -0,0 +1,14 @@
+package com.mwamwayababmeokuh.mwamwaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MwamwaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwamwaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mwamwayababmeokuh/mwamwaspring/MwamwaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwamwayababmeokuh.mwamwaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwamwaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
